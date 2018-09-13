3c332535fc5cdde49f865a8d9a2507b29ce20e39
llmlks <lamelkas@gmail.com>
2018-09-13 13:22:23 +0300

    Change deployment script to point to correct files

--- scripts/before-deploy.sh
+++ scripts/before-deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 
-openssl aes-256-cbc -K $encrypted_127facfaf176_key -iv $encrypted_127facfaf176_iv -in gke-service-account.json.enc -out gke-service-account.json -d
+openssl aes-256-cbc -K $encrypted_127facfaf176_key -iv $encrypted_127facfaf176_iv -in ../gke-service-account.json.enc -out gke-service-account.json -d
 
 if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
   rm -rf $HOME/google-cloud-sdk;
