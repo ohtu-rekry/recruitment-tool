e53078b2af2bff35b4b0bab1d8b82fd8c0694f4e
Jerry Tamlander <jerry.tamlander@helsinki.fi>
2018-09-13 09:32:51 +0300

    Created route for recruiter and some fixes for database connection

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,10 +1,6 @@
-const fs = require('fs')
-const path = require('path')
 const Sequelize = require('sequelize')
-const basename = path.basename(module.filename)
 const env = process.env.NODE_ENV || 'development'
 const config = require(`${__dirname}/../config/config.js`)[env]
-const db = {}
 
 let sequelize
 if (config.use_env_variable) {
@@ -15,24 +11,22 @@ if (config.use_env_variable) {
   )
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file =>
-    (file.indexOf('.') !== 0) &&
-    (file !== basename) &&
-    (file.slice(-3) === '.js'))
-  .forEach(file => {
-    const model = sequelize.import(path.join(__dirname, file))
-    db[model.name] = model
-  })
+const models = {
+  Recruiter: sequelize.import('./recruiter'),
+}
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db)
+Object.keys(models).forEach(key => {
+  if ('associate' in models[key]) {
+    models[key].associate(models)
   }
 })
 
-db.sequelize = sequelize
-db.Sequelize = Sequelize
+sequelize.sync()
+  .then(() => {
+    console.log('Tables created')
+  })
 
-module.exports = db
+models.sequelize = sequelize
+models.Sequelize = Sequelize
+
+module.exports = models
--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,8 @@ const http = require('http')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
+const recruiterRouter = require('../controllers/recruiterRouter')
+
 const PORT = process.env.port || 8080
 const HOST = '0.0.0.0'
 
@@ -10,13 +12,12 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => [
+app.get('/', (req, res) => {
   res.send('Hello world! \n')
-])
-
-app.get('/api/users', (req,res) => {
-  res.send('USER PAGE')
 })
+
+app.use('/api/recruiters', recruiterRouter)
+
 
 const server = http.createServer(app)
 
