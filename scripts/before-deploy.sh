95bfd67d8c5fa2feca68fa0853bb21fa85ba26fe
llmlks <lamelkas@gmail.com>
2018-09-13 15:41:15 +0300

    Use new Google Cloud SDK when installing kubectl in Travis

--- scripts/before-deploy.sh
+++ scripts/before-deploy.sh
@@ -6,13 +6,12 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 openssl aes-256-cbc -K $encrypted_127facfaf176_key -iv $encrypted_127facfaf176_iv -in ../gke-service-account.json.enc -out gke-service-account.json -d
 
-if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
-  rm -rf $HOME/google-cloud-sdk;
+if [ ! -d ${HOME}/google-cloud-sdk/bin ]; then
+  rm -rf ${HOME}/google-cloud-sdk;
   curl https://sdk.cloud.google.com | bash >/dev/null;
 fi
 
-source /home/travis/google-cloud-sdk/path.bash.inc
+source ${HOME}/google-cloud-sdk/path.bash.inc
 
-gcloud --quiet version
-gcloud --quiet components update
-gcloud --quiet components install kubectl
+gcloud version
+gcloud components update kubectl
