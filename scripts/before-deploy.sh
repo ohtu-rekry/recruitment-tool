9c3ada080177def65b77b1627a376373a8ec44e4
llmlks <lamelkas@gmail.com>
2018-09-13 14:42:23 +0300

    Send output to >/dev/null from installing Goodle Cloud SDK on Travis

--- scripts/before-deploy.sh
+++ scripts/before-deploy.sh
@@ -8,7 +8,7 @@ openssl aes-256-cbc -K $encrypted_127facfaf176_key -iv $encrypted_127facfaf176_i
 
 if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
   rm -rf $HOME/google-cloud-sdk;
-  curl https://sdk.cloud.google.com | bash;
+  curl https://sdk.cloud.google.com | bash >/dev/null;
 fi
 
 source /home/travis/google-cloud-sdk/path.bash.inc
