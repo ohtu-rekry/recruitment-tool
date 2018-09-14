020c386530233e0263de092eae86277920369302
Jerry Tamlander <jerry.tamlander@helsinki.fi>
2018-09-14 09:28:37 +0300

    Fixed database settings so it works now with docker-compose

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -6,21 +6,29 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+/*
+const DB_USERNAME = process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASS
 const DB_NAME = process.env.DB_NAME
-const DB_PASS = process.env.DB_PASS
-
-let sequelize
+*/
+/*let sequelize
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable])
 } else {
   sequelize = new Sequelize(
-    `postgres://${DB_NAME}:${DB_PASS}@localhost/ohturekry`
+    config.database, config.username, config.password, config
+    `postgres://${DB_USERNAME}:${DB_PASSWORD}@localhost/${DB_NAME}`
   )
-}
+} */
+
+let sequelize = new Sequelize(
+  `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`
+)
 
 const models = {
   Recruiter: sequelize.import('./recruiter'),
 }
+
 
 Object.keys(models).forEach(key => {
   if ('associate' in models[key]) {
