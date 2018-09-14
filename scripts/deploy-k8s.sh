d595f302d193a2d9e16575c4c2b4a533f42123f0
llmlks <lamelkas@gmail.com>
2018-09-14 13:47:36 +0300

    Try a new approach to deploying on Travis

--- scripts/deploy-k8s.sh
+++ scripts/deploy-k8s.sh
@@ -3,11 +3,22 @@
 set -e
 
 npm prune --production
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+openssl aes-256-cbc -K $encrypted_127facfaf176_key -iv $encrypted_127facfaf176_iv -in ../gke-service-account.json.enc -out gke-service-account.json -d
+
+if [ ! -d ${HOME}/google-cloud-sdk/bin ]; then
+  rm -rf ${HOME}/google-cloud-sdk;
+  curl https://sdk.cloud.google.com | bash >/dev/null;
+fi
+
+source ${HOME}/google-cloud-sdk/path.bash.inc
+
+gcloud update components kubectl
+
 docker build -t ${DOCKER_IMAGE}:${TRAVIS_BUILD_NUMBER} .
 docker push ${DOCKER_IMAGE}:${TRAVIS_BUILD_NUMBER}
-
-
-gcloud version
 
 gcloud auth activate-service-account --key-file=gke-service-account.json
 gcloud config set project emblica-212815
