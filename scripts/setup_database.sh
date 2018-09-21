8fbcb733f8ef9380f9681605f62c9c9a5dd1eee9
llmlks <lamelkas@gmail.com>
2018-09-21 11:21:22 +0300

    Fix issues with postgres authentication in Travis

--- scripts/setup_database.sh
+++ scripts/setup_database.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+sudo sed -i -e '/local.*peer/s/postgres/all/' -e 's/peer\|md5/trust/g' /etc/postgresql/*/main/pg_hba.conf
+sudo service postgresql restart
+sleep 1
+
 psql -c "CREATE DATABASE ${DB_NAME};" -U postgres
 psql -c "CREATE USER ${DB_USERNAME} WITH PASSWORD '${DB_PASSWORD}';" -U postgres
 
