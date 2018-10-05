98eb8ee868f8f66c75432e3be7cd340301a64db6
Ollikehy <eragah@gmail.com>
2018-10-05 15:47:28 +0300

    fix lint

--- client/src/components/Buttons.js
+++ client/src/components/Buttons.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 import PropTypes from 'prop-types'
 
-export const LinkButton = ({link, text}) => {
+export const LinkButton = ({ link, text }) => {
   return (
     <div>
       <Button variant='contained' href={link} size='small'>{text}</Button>
