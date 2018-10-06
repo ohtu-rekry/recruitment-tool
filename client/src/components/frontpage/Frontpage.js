32aa9da6d932cd707baea76823a01fddc0ffbd0f
Jerry Tamlander <jerry.tamlander@helsinki.fi>
2018-10-06 17:44:22 +0300

    Use master branch's frontpage

--- client/src/components/frontpage/Frontpage.js
+++ client/src/components/frontpage/Frontpage.js
@@ -1,23 +1,64 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+import { fetchJobPostings } from '../../redux/actions/actions'
+import Header from './Header'
+import JobPosting from './JobPosting'
 
 class FrontPage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      fireRedirect: false,
+      jobPostingId: null
+    }
+    this.handleJobPostingClick = this.handleJobPostingClick.bind(this)
+  }
+
+  componentDidMount() {
+    const { fetchJobPostings } = this.props
+    fetchJobPostings()
+  }
+
+  handleJobPostingClick(id) {
+    this.setState({
+      fireRedirect: true,
+      jobPostingId: id
+    })
+  }
 
   render() {
-    return(
-      <div>
-        Welcome to the frontpage!
+    if (this.state.fireRedirect) {
+      return <Redirect to={`/jobposting/${this.state.jobPostingId}`} />
+    }
+    return (
+      <div className='frontpage'>
+        <Header />
+        <div className='job-postings'>
+          <div className='job-postings__list' >
+            {this.props.jobPostings.map(posting =>
+              <JobPosting key={posting.id} data={posting} onClick={() => this.handleJobPostingClick(posting.id)} />
+            )}
+          </div>
+        </div>
       </div>
     )
   }
 }
 
+FrontPage.protoTypes = {
+  jobPostings: PropTypes.array.isRequired
+}
+
 const mapStateToProps = (state) => ({
-  loggedIn : state.loginReducer.loggedIn
+  jobPostings: state.jobPostingReducer.jobPostings
 })
 
 const mapDispatchToProps = {
-
+  fetchJobPostings
 }
 
 export default connect(
