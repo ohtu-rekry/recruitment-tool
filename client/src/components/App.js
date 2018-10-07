c5563a28eab3e56b609698f7eb336cbd37e2aa5d
Jerry Tamlander <jerry.tamlander@helsinki.fi>
2018-10-07 15:24:27 +0300

    Restored original front page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import FrontPage from './frontpage/FrontPage'
 
 
 class App extends Component {
   render() {
     return (
       <div className='container'>
-
+        <FrontPage />
       </div>
     )
   }
