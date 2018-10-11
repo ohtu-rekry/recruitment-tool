fc74f6349f7a41340e89f5a119a58774375cbc06
llmlks <lamelkas@gmail.com>
2018-10-11 10:12:49 +0300

    Remove unnecessary JobPosting component

--- client/src/components/frontpage/JobPostingListing.js
+++ client/src/components/frontpage/JobPostingListing.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-import JobPosting from './JobPosting'
+import JobPosting from './'
 
 const JobPostingListing = (props) => {
   return (
@@ -10,7 +10,13 @@ const JobPostingListing = (props) => {
       key={props.data.id}
       className='job-posting__link' >
 
-      <JobPosting data={props.data} />
+      <div className='job-posting-listing'>
+        <div className='job-posting-listing__title'>
+          <h2>{props.data.title}</h2>
+        </div>
+        <div className='job-posting-listing__content'>
+        </div>
+      </div>
 
     </Link>
   )
