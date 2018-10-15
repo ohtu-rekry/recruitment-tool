66c11cd204c213994f5f151a2f6bc7e5696e768b
Andrea Eriksson <andreaeriksson153@gmail.com>
2018-10-15 14:15:10 +0300

    allowNull:false in models

--- server/db/models/postingstage.js
+++ server/db/models/postingstage.js
@@ -2,18 +2,19 @@
 module.exports = (sequelize, DataTypes) => {
 
   const PostingStage = sequelize.define('PostingStage', {
-    stageName: DataTypes.STRING,
-    orderNumber: { type: DataTypes.INTEGER, allowNull: false }
+    stageName: {type: DataTypes.STRING, allowNull: false},
+    orderNumber: {type: DataTypes.INTEGER, allowNull: false}
   }, {})
 
   PostingStage.associate = function (models) {
     PostingStage.hasMany(models.JobApplication, {
-      foreignKey: 'postingStageId',
+      foreignKey: {name: 'postingStageId', allowNull: false },
+      onDelete: 'CASCADE',
       as: 'jobApplications'
     })
 
     PostingStage.belongsTo(models.JobPosting, {
-      foreignKey: 'jobPostingId',
+      foreignKey: { name: 'jobPostingId', allowNull: false },
       onDelete: 'CASCADE',
       hooks: true
     })
