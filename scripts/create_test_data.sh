ecdc7396b5b839b4b8256179c4d565673d1aa1d8
Laila Melkas <llmlks@users.noreply.github.com>
2018-10-16 11:09:07 +0300

    Delete unused script for creating test data
    The test data creation happens in the init.sh script

--- scripts/create_test_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker exec -it $(docker ps | grep server | awk '{ print $1 }') /bin/sh -c 'npm run db:migrate'
-docker exec -it $(docker ps | grep server | awk '{ print $1 }') /bin/sh -c 'npm run db:seed'
