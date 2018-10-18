3758589d877511a18c166493f612eae7b24f78f4
Andrea Eriksson <andreaeriksson153@gmail.com>
2018-10-18 21:51:44 +0300

    Feature: change posting stage of job application backend

--- server/src/controllers/jobApplicationRouter.js
+++ server/src/controllers/jobApplicationRouter.js
@@ -2,25 +2,44 @@ const jobApplicationRouter = require('express').Router()
 const { JobApplication, PostingStage, Recruiter } = require('../../db/models')
 const jwt = require('jsonwebtoken')
 
-jobApplicationRouter.get('/', async (req, res) => {
+const adminIsLoggedIn = (token) => {
   try {
-
-    const token = req.token
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
-
     if (!token || !decodedToken.username) {
       console.log(token)
       console.log(decodedToken)
-      return res.status(401).json({ error: 'Operation unauthorized' })
+      return null
     }
 
+    return decodedToken.username
+  } catch (e) { throw e }
+}
+
+const recruiterFoundInDb = async (username) => {
+  try {
     const recruiter = await Recruiter.findOne({
       where: {
-        username: decodedToken.username
+        username: username
       }
     })
 
     if (!recruiter) {
+      return false
+    }
+    return true
+  } catch (e) { throw e }
+}
+
+jobApplicationRouter.get('/', async (req, res) => {
+  try {
+
+    const adminUsername = adminIsLoggedIn(req.token)
+    if (!adminUsername) {
+      return res.status(401).json({ error: 'Operation unauthorized' })
+    }
+
+    const recruiterExists = await recruiterFoundInDb(adminUsername)
+    if (!recruiterExists) {
       return res.status(500).json({ error: 'Logged in user not found in database' })
     }
 
@@ -53,13 +72,13 @@ jobApplicationRouter.post('/', async (req, res) => {
     }
 
     const firstPostingStage = await PostingStage.findOne({
-      where : {
+      where: {
         jobPostingId: body.jobPostingId,
         orderNumber: 0
       }
     })
 
-    if(!firstPostingStage) {
+    if (!firstPostingStage) {
       return res.status(500).json({ error: 'Could not find posting stage' })
     }
 
@@ -74,6 +93,55 @@ jobApplicationRouter.post('/', async (req, res) => {
   } catch (e) {
     console.log(e)
     res.status(500).json({ error: 'Something went wrong with new job application' })
+  }
+})
+
+jobApplicationRouter.patch('/', async (req, res) => {
+  try {
+    const jobApplicationId = req.body.jobApplicationId
+    const postingStageId = req.body.postingStageId
+
+    const adminUsername = adminIsLoggedIn(req.token)
+    if (!adminUsername) {
+      return res.status(401).json({ error: 'Operation unauthorized' })
+    }
+
+    const recruiterExists = await recruiterFoundInDb(adminUsername)
+    if (!recruiterExists) {
+      return res.status(500).json({ error: 'Logged in user not found in database' })
+    }
+
+    if (!jobApplicationId) {
+      return res.status(400).json({ error: 'Job application must be defined' })
+    }
+
+    if (!postingStageId) {
+      return res.status(400).json({ error: 'Posting stage must be defined' })
+    }
+
+    const postingStage = await PostingStage.findOne({ where: { id: postingStageId } })
+    if (!postingStage) {
+      return res.status(400).json({ error: 'Could not find posting stage' })
+    }
+
+    const resultOfUpdate = await JobApplication.update(
+      { postingStageId },
+      { where: { id: jobApplicationId }, returning: true }
+    )
+
+    if (resultOfUpdate[0] !== 1) {
+      return res.status(500).json({ error: 'Did not update posting stage of job application' })
+    }
+
+    res.status(200).json(resultOfUpdate[1][0])
+
+  } catch (e) {
+    if (e.name === 'JsonWebTokenError') {
+      res.status(401).json({ error: e.message })
+    } else {
+      console.log(e)
+      res.status(500).json({ error: 'Something went wrong when trying to change application' })
+    }
   }
 })
 
