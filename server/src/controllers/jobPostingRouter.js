26d92e6113e04d6280d73bb944d2d4637ea5dd4e
llmlks <lamelkas@gmail.com>
2018-11-05 15:10:12 +0200

    Backend support for editing a job posting

--- server/src/controllers/jobPostingRouter.js
+++ server/src/controllers/jobPostingRouter.js
@@ -2,13 +2,14 @@ const jobPostingRouter = require('express-promise-router')()
 const { JobPosting, Recruiter, PostingStage, JobApplication } = require('../../db/models')
 const jwt = require('jsonwebtoken')
 const { jwtMiddleware } = require('../../utils/middleware')
-const { jobPostingValidator } = require('../../utils/validators')
+const { jobPostingValidator, postingPutValidator } = require('../../utils/validators')
+const Sequelize = require('sequelize')
 
 jobPostingRouter.get('/', async (req, res) => {
   return await JobPosting.findAll().then(jobpostings => res.json(jobpostings))
 })
 
-jobPostingRouter.post('/', jwtMiddleware, jobPostingValidator,  async (req, res) => {
+jobPostingRouter.post('/', jwtMiddleware, jobPostingValidator, async (req, res) => {
   const body = req.body
 
   const decodedToken = jwt.verify(req.token, process.env.JWT_SECRET)
@@ -26,13 +27,16 @@ jobPostingRouter.post('/', jwtMiddleware, jobPostingValidator,  async (req, res)
   })
 
   try {
-    await Promise.all(body.stages.map((stage, index) =>
-      PostingStage.create({
-        stageName: stage.stageName,
-        orderNumber: index,
-        jobPostingId: posting.id
-      })
-    ))
+    await Promise.all(
+      body.stages
+        .filter(stage => stage.stageName)
+        .map((stage, index) =>
+          PostingStage.create({
+            stageName: stage.stageName,
+            orderNumber: index,
+            jobPostingId: posting.id
+          })
+        ))
     res.status(201).json(posting)
   } catch (error) {
     await JobPosting.destroy({
@@ -70,6 +74,80 @@ jobPostingRouter.get('/:id/applicants', jwtMiddleware, async (req, res) => {
     )
 
     res.status(200).json(stagesWithApplicants)
+  } catch (error) {
+    throw error
+  }
+})
+
+jobPostingRouter.put('/:id', jwtMiddleware, postingPutValidator, async (request, response) => {
+
+  try {
+    const body = request.body
+    const postingId = request.params.id
+    const posting = await JobPosting.findById(postingId)
+
+    if (!posting) {
+      return response.status(400).json({ error: 'Invalid job posting ID' })
+    }
+
+    const existingStages = await PostingStage.findAll({
+      where: {
+        id: {
+          [Sequelize.Op.in]: body.stages.map(stage => stage.id)
+        }
+      }
+    })
+
+    const newStages = body.stages.filter(stage =>
+      !(existingStages
+        .map(existing => existing.id)
+        .includes(stage.id)))
+
+    const deletedStages = await PostingStage.findAll({
+      where: {
+        jobPostingId: postingId,
+        id: {
+          [Sequelize.Op.notIn]: body.stages.map(stage => stage.id)
+        }
+      },
+      include: [ 'jobApplications' ]
+    })
+
+    const updatedPosting = await posting.update({
+      title: body.title,
+      content: body.content,
+      showFrom: body.showFrom,
+      showTo: body.showTo
+    })
+
+    await Promise.all(
+      newStages
+        .filter(stage => stage.stageName)
+        .map((stage, index) =>
+          PostingStage.create({
+            stageName: stage.stageName,
+            orderNumber: stage.orderNumber || index,
+            jobPostingId: postingId
+          })
+        ))
+
+    await Promise.all(deletedStages
+      .filter(stage => stage.jobApplications.length === 0)
+      .map((stage) =>
+        PostingStage.destroy({
+          where: {
+            id: stage.id
+          }
+        })
+      ))
+
+    response.status(200).json({
+      ...updatedPosting.toJSON(),
+      stages: await PostingStage.findAll({
+        where: { jobPostingId: postingId }
+      })
+    })
+
   } catch (error) {
     throw error
   }
