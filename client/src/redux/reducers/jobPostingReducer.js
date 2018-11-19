7cdc7f1e4afe9cc31a1dc769426cac6cdffef16b
Andrea Eriksson <andreaeriksson153@gmail.com>
2018-11-19 17:00:21 +0200

    Improvement: Deleting stage with applications prevented in frontend

--- client/src/redux/reducers/jobPostingReducer.js
+++ client/src/redux/reducers/jobPostingReducer.js
@@ -1,7 +1,8 @@
 import { handleActions } from 'redux-actions'
 import * as actions from '../actions/actions'
 
-const defaultStages = [{ stageName: 'Applied', canRemove: false }, { stageName: 'Accepted', canRemove: false }, { stageName: 'Rejected', canRemove: false }]
+const defaultStageNames = ['Applied', 'Accepted', 'Rejected']
+const defaultStages = defaultStageNames.map((name) => ({ stageName: name, canRemove: false }))
 const creationSuccessMessage = 'Job posting successfully added'
 
 const initialState = {
@@ -10,7 +11,8 @@ const initialState = {
   creationRequestStatus: null,
   copiedStages: null,
   showFrom: {},
-  showTo: {}
+  showTo: {},
+  defaultStageNames: defaultStageNames
 }
 
 const reducer = handleActions(
@@ -53,11 +55,10 @@ const reducer = handleActions(
     [actions.setStages]: (state, action) => ({
       ...state,
       jobPostingStages: action.payload.stages
-        .sort((a, b) => a.orderNumber - b.orderNumber)
         .map((stage) => {
-          const defaultStageNames = defaultStages.map(stage => stage.stageName)
-          const isNotDefaultStage = !defaultStageNames.includes(stage.stageName)
-          return ({ ...stage, canRemove: isNotDefaultStage })
+          const isRemovable = !defaultStageNames.includes(stage.stageName)
+                              && stage.jobApplications.length === 0
+          return ({ ...stage, canRemove: isRemovable })
         }),
     }),
     [actions.clearStages]: (state, action) => ({
