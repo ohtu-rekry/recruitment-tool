6441e3447a90f7da9abcb2ba48c8e03191260c29
llmlks <lamelkas@gmail.com>
2018-11-19 13:56:14 +0200

    Frontend implementation for adding comments to an application

--- client/src/redux/apis/jobApplicationApi.js
+++ client/src/redux/apis/jobApplicationApi.js
@@ -3,17 +3,29 @@ import axios from 'axios'
 export const root = '/api/jobapplication'
 
 export default class jobApplicationApi {
+
   static get(payload) {
     const config = {
       headers: { 'Authorization': 'bearer ' + payload.token }
     }
-    return axios.get(root, config) }
+    return axios.get(root, config)
+  }
+
   static add(payload) {
-    return axios.post(root, payload) }
+    return axios.post(root, payload)
+  }
+
   static moveApplicants(payload) {
     const config = {
       headers: { 'Authorization': 'bearer ' + payload.token }
     }
     return axios.patch(root, payload.data, config)
+  }
+
+  static addComment(payload) {
+    const config = {
+      headers: { 'Authorization': `bearer ${payload.token}` }
+    }
+    return axios.post(`${root}/${payload.applicationId}/comment`, payload.data, config)
   }
 }
