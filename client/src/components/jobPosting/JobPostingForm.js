83f1d8f24086ce05b6d97b13c4d77a74abc368b6
ollikehy <eragah@gmail.com>
2018-12-13 12:18:02 +0200

    remove whitespace

--- client/src/components/jobPosting/JobPostingForm.js
+++ client/src/components/jobPosting/JobPostingForm.js
@@ -29,7 +29,7 @@ export class JobPostingForm extends Component {
 
   componentDidMount() {
     if (this.state.mode === 'edit') {
-      const jobPostingId = this.props.match.params.id ? this.props.match.params.id : window.location.href.split('/')[4]	
+      const jobPostingId = this.props.match.params.id ? this.props.match.params.id : window.location.href.split('/')[4]
       this.setState({ id: jobPostingId })
       this.props.fetchJobPostingWithStages(jobPostingId)
     }
