8b3078f251b58ebc0477c7fe77d8130c35033450
Lotta Merisaari <merisaari.lotta@gmail.com>
2018-12-16 19:29:49 +0200

    Remove redundant action

--- client/src/redux/actions/generalActions.js
+++ client/src/redux/actions/generalActions.js
@@ -6,7 +6,5 @@ export const fetchJobPostings = createAction('FETCH_JOBPOSTINGS')
 export const setJobPostings = createAction('SET_JOBPOSTINGS')
 
 export const getAllApplicants = createAction('GET_ALL_APPLICANTS')
-export const getApplicantsSuccess = createAction('GET_APPLICANTS_SUCCESS')
-
 
 export const updateStages = createAction('UPDATE_STAGES')
